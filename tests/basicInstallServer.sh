cdf3cf132646465dd193a4cbd6689f87e0e82c06
Dale Henrichs <dale.henrichs@gemtalksystems.com>
2015-10-16 14:16:38 -0700

    fix another typo ... test fiddle

--- tests/basicInstallServer.sh
+++ tests/basicInstallServer.sh
@@ -7,8 +7,14 @@ set -e  # exit on error
 
 # install server
 $GS_HOME/bin/private/clone_gs_client_dev -c https
-pushd $GS_HOME/dev
-  git checkout dev
+
+pushd $GS_HOME
+  theBranchName=`git rev-parse --abbrev-ref HEAD`
+  pushd $GS_HOME/dev
+    if [ "$theBranchName" = "master" ] ; then
+      git checkout dev
+    fi
+  popd
 popd
 installServerClient -o GsDevKit ${STONENAME1} tode $GS_VERSION
 
