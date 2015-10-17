9887e39cfecf23885a523d45cd326036b1ee898d
Dale Henrichs <dale.henrichs@gemtalksystems.com>
2015-10-17 08:54:45 -0700

    move the travis-specific sys/default/client/tode-scripts to the test structure and then copy them to sys/local/client/tode-scripts dir on travis ... remove a number of GS_TRAVIS sites

--- tests/todeDevBranch.sh
+++ tests/todeDevBranch.sh
@@ -7,6 +7,9 @@ exit 0 # skip this step for now ...
 
 set -e  # exit on error
 
+# no backups on travis
+cp $GS_HOME/tests/sys/local/client/tode-scripts* $GS_HOME/sys/local/client/tode-scripts
+ 
 # dev branch of tODE
 cp $GS_HOME/tests/projects/magritte3/install.ston $GS_HOME/projects/magritte3
 cp $GS_HOME/tests/projects/pier3/install300.ston $GS_HOME/projects/pier3
