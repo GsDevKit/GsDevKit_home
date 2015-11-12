cd939833fb67a826895845f28673cf082677583f
Damien Cassou <damien@cassou.me>
2015-11-12 10:07:17 -0300

    Use standard shebang

--- tests/basicInstallServer.sh
+++ tests/basicInstallServer.sh
@@ -1,9 +1,9 @@
-#!/bin/bash -x
+#!/usr/bin/env bash
 #
 # Copyright (c) 2015 GemTalk Systems, LLC. All Rights Reserved <dhenrich@gemtalksystems.com>.
 #
 
-set -e  # exit on error
+set -xe  # print commands and exit on error
 
 # install server
 installServerClient -o GsDevKit ${STONENAME1} tode $GS_VERSION
