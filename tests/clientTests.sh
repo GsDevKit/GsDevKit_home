e5e63d645279bef614f8df2d4c9953565f806dad
Dale Henrichs <dale.henrichs@gemtalksystems.com>
2016-04-27 09:55:10 -0700

    #95 convert the tests scripts

--- tests/clientTests.sh
+++ tests/clientTests.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
+#=========================================================================
+# Copyright (c) 2015, 2016 GemTalk Systems, LLC. All Rights Reserved <dhenrich@gemtalksystems.com>.
 #
-# Copyright (c) 2015 GemTalk Systems, LLC. All Rights Reserved <dhenrich@gemtalksystems.com>.
-#
+#   MIT license: https://github.com/GsDevKit/GsDevKit_home/blob/master/license.txt
+#=========================================================================
 
 set -xe  # print commands and exit on error
 
