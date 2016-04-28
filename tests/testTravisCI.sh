9ac81c0d08479a0219d83bc3db4464d2aa18ef4f
Dale Henrichs <dale.henrichs@gemtalksystems.com>
2016-04-27 20:41:45 -0700

    remove the cat

--- tests/testTravisCI.sh
+++ tests/testTravisCI.sh
@@ -89,8 +89,8 @@ EOF
 EOF
     status=$?
     if [ "$status" != "0" ] ; then
-       # cat $GS_HOME/server/stones/$upgradeStoneName/upgradeLog/topazerrors.log
-      cat $GS_HOME/server/stones/$upgradeStoneName/upgradeLog/upgradeImage.out
+      # cat $GS_HOME/server/stones/$upgradeStoneName/upgradeLog/topazerrors.log
+      # cat $GS_HOME/server/stones/$upgradeStoneName/upgradeLog/upgradeImage.out
       exit 1
     else
       exit 0
