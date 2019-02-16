7ee04b82a5e3bf313adc0c600b2b424ee918adb7
Dale Henrichs <dale.henrichs@gemtalksystems.com>
2019-02-16 11:23:50 -0800

    Issue #240: typo

--- tests/unitTests.sh
+++ tests/unitTests.sh
@@ -22,18 +22,18 @@ startTopaz "$1" -l << EOF
   TestCase suite tests do: [ :s | suite addTests: s tests ].
 	res := suite run.
 
- 	GsFile logServer: 'Unit tests for $1, GemStone version ', (System gemVersionAt: #gsVersion) printString.
-	GsFile logServer: res printString.
-	GsFile logServer: '  errors'.
+ 	GsFile gciLogServer: 'Unit tests for $1, GemStone version ', (System gemVersionAt: #gsVersion) printString.
+	GsFile gciLogServer: res printString.
+	GsFile gciLogServer: '  errors'.
 	passed := res errors isEmpty.
 	(res errors collect: [:each | each printString ]) asArray sort do: [:each |
-		GsFile logServer: '	', each ].
+		GsFile gciLogServer: '	', each ].
 	res failures size = 0
 		ifTrue: [ ^ passed ].
-	GsFile logServer: '  failures'.
+	GsFile gciLogServer: '  failures'.
 	passed := passed & res errors isEmpty.
 	(res failures collect: [:each | each printString]) asArray sort do: [:each |
-		GsFile logServer: '	',each ].
+		GsFile gciLogServer: '	',each ].
 	^ passed
 %
 
