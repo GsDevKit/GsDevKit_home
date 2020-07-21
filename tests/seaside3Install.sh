36b7232a08fa09de3da1c6efb2a6ff87ae515dae
Dale Henrichs <dale.henrichs@gemtalksystems.com>
2020-07-21 12:43:52 -0700

    comment out the older seaside monkey business [ci skip]

--- tests/seaside3Install.sh
+++ tests/seaside3Install.sh
@@ -31,12 +31,12 @@ startNetldi ${STONENAME1} -d -g -a $USER "${STONENAME1}_ldi"
 "$GS_HOME/bin/private/gsDevKitTodeCommandLine" todeIt ${STONENAME1} << EOF
 # after test run, <self> will be a TestResult
 project install --url=http://gsdevkit.github.io/GsDevKit_home/Seaside32.ston
-git checkout 9f9a232e2 $GS_HOME/shared/repos/Seaside
+# git checkout 9f9a232e2 $GS_HOME/shared/repos/Seaside
 project load --loads=\`#('CI')\` Seaside3
-eval \`TestCase compileMethod: 'run: aResult Transcript cr; show: self printString. aResult runCase: self '\`
+# eval \`TestCase compileMethod: 'run: aResult Transcript cr; show: self printString. aResult runCase: self '\`
 # eval \`WAGemStoneProcessEnvironmentWrapper compileMethod: 'isProcessEnvironmentWrapper ^false'\`
 # eval \`WAPartialContinuationAlternatMarkerTest compileMethod: 'isProcessEnvironmentWrapper ^false'\`
-eval \`System commit\`
+# eval \`System commit\`
 test --batch project Seaside3
 eval \`[(self hasErrors or: [ self hasFailures ]) ifTrue: [ self error: 'Tests failed' ] ] on: Warning do: [:ex | ex resume: true ]\`
 EOF
