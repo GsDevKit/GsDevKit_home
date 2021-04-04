e9620a099a7a085680b39d7918e45df329696c55
Dale Henrichs <dale.henrichs@gemtalksystems.com>
2021-04-04 11:55:41 -0700

    Issue #310, comment out all of the tests run after the first createStone so we can cycle through tests quicker

--- tests/basicTestB.sh
+++ tests/basicTestB.sh
@@ -9,60 +9,60 @@ set -xe  # print commands and exit on error
 
 
 createStone ${STONENAME2} $GS_VERSION
-createStone -g ${STONENAME3} $GS_VERSION
-
-pushd $GS_HOME/server/stones/${STONENAME2}
-	cat - >> .topazini << EOF
-# set user SystemUser
-# set password banana
-EOF
-popd
-
-stopAllStones
-startAllStones
-stopAllStones -b
-startAllStones -b
-#deleteStone ${STONENAME2}
-deleteStone ${STONENAME3}
-
-startStone -b ${STONENAME1}
-set +e
-gitusername=`git config user.name`
-if [ "${gitusername}x" = "x" ] ; then
-	# make sure git user.name and user.email are set
-	git config --global user.email "travis@example.com"
-	git config --global user.name "Travis Ci"
-fi
-set -e
-todeIt ${STONENAME1} project new Foo
-
-createStone -f -u http://gsdevkit.github.io/GsDevKit_home/TestSample2.ston \
-            -i Sample -l Sample \
-            -t $todeSnapshot ${STONENAME3} $GS_VERSION
-
-$GS_HOME/bin/private/gsDevKitTodeCommandLine todeIt ${STONENAME3} << EOF
-project entry --url=http://gsdevkit.github.io/GsDevKit_home/Flow.ston /sys/stone/projects
-project install Flow
-project install --local --url=http://gsdevkit.github.io/GsDevKit_home/GLASS1.ston
-project install --local --url=http://gsdevkit.github.io/GsDevKit_home/Grease.ston
-project install --local --url=http://gsdevkit.github.io/GsDevKit_home/GsApplicationTools.ston
-project install --local --url=http://gsdevkit.github.io/GsDevKit_home/GsDevKit_home.ston
-project install --local --url=http://gsdevkit.github.io/GsDevKit_home/Magritte3.ston
-project install --local --url=http://gsdevkit.github.io/GsDevKit_home/Mapless.ston
-project install --local --url=http://gsdevkit.github.io/GsDevKit_home/Metacello.ston
-project install --local --url=http://gsdevkit.github.io/GsDevKit_home/NeoJSON.ston
-project install --local --url=http://gsdevkit.github.io/GsDevKit_home/PharoCompatibility.ston
-project install --local --url=http://gsdevkit.github.io/GsDevKit_home/Sample.ston
-project install --local --url=http://gsdevkit.github.io/GsDevKit_home/Seaside32.ston
-project install --local --url=http://gsdevkit.github.io/GsDevKit_home/ServiceVM.ston
-project install --local --url=http://gsdevkit.github.io/GsDevKit_home/SIXX.ston
-project install --local --url=http://gsdevkit.github.io/GsDevKit_home/Ston.ston
-project install --local --url=http://gsdevkit.github.io/GsDevKit_home/TestSample2.ston
-project install --local --url=http://gsdevkit.github.io/GsDevKit_home/Tode.ston
-project install --local --url=http://gsdevkit.github.io/GsDevKit_home/XMLSupport.ston
-project install --local --url=http://gsdevkit.github.io/GsDevKit_home/ZincHTTPComponents.ston
-EOF
-
-. $GS_HOME/bin/defStone.env ${STONENAME1}
-cd $GS_HOME/server/stones/${STONENAME1}
-. defStone.env
+#createStone -g ${STONENAME3} $GS_VERSION
+#
+#pushd $GS_HOME/server/stones/${STONENAME2}
+#	cat - >> .topazini << EOF
+## set user SystemUser
+## set password banana
+#EOF
+#popd
+#
+#stopAllStones
+#startAllStones
+#stopAllStones -b
+#startAllStones -b
+##deleteStone ${STONENAME2}
+#deleteStone ${STONENAME3}
+#
+#startStone -b ${STONENAME1}
+#set +e
+#gitusername=`git config user.name`
+#if [ "${gitusername}x" = "x" ] ; then
+#	# make sure git user.name and user.email are set
+#	git config --global user.email "travis@example.com"
+#	git config --global user.name "Travis Ci"
+#fi
+#set -e
+#todeIt ${STONENAME1} project new Foo
+#
+#createStone -f -u http://gsdevkit.github.io/GsDevKit_home/TestSample2.ston \
+#            -i Sample -l Sample \
+#            -t $todeSnapshot ${STONENAME3} $GS_VERSION
+#
+#$GS_HOME/bin/private/gsDevKitTodeCommandLine todeIt ${STONENAME3} << EOF
+#project entry --url=http://gsdevkit.github.io/GsDevKit_home/Flow.ston /sys/stone/projects
+#project install Flow
+#project install --local --url=http://gsdevkit.github.io/GsDevKit_home/GLASS1.ston
+#project install --local --url=http://gsdevkit.github.io/GsDevKit_home/Grease.ston
+#project install --local --url=http://gsdevkit.github.io/GsDevKit_home/GsApplicationTools.ston
+#project install --local --url=http://gsdevkit.github.io/GsDevKit_home/GsDevKit_home.ston
+#project install --local --url=http://gsdevkit.github.io/GsDevKit_home/Magritte3.ston
+#project install --local --url=http://gsdevkit.github.io/GsDevKit_home/Mapless.ston
+#project install --local --url=http://gsdevkit.github.io/GsDevKit_home/Metacello.ston
+#project install --local --url=http://gsdevkit.github.io/GsDevKit_home/NeoJSON.ston
+#project install --local --url=http://gsdevkit.github.io/GsDevKit_home/PharoCompatibility.ston
+#project install --local --url=http://gsdevkit.github.io/GsDevKit_home/Sample.ston
+#project install --local --url=http://gsdevkit.github.io/GsDevKit_home/Seaside32.ston
+#project install --local --url=http://gsdevkit.github.io/GsDevKit_home/ServiceVM.ston
+#project install --local --url=http://gsdevkit.github.io/GsDevKit_home/SIXX.ston
+#project install --local --url=http://gsdevkit.github.io/GsDevKit_home/Ston.ston
+#project install --local --url=http://gsdevkit.github.io/GsDevKit_home/TestSample2.ston
+#project install --local --url=http://gsdevkit.github.io/GsDevKit_home/Tode.ston
+#project install --local --url=http://gsdevkit.github.io/GsDevKit_home/XMLSupport.ston
+#project install --local --url=http://gsdevkit.github.io/GsDevKit_home/ZincHTTPComponents.ston
+#EOF
+#
+#. $GS_HOME/bin/defStone.env ${STONENAME1}
+#cd $GS_HOME/server/stones/${STONENAME1}
+#. defStone.env
